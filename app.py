c36179b48b1b0f90208257275f4d7dd5e01a327d
bandrewv <brock.vriesman@gmail.com>
2020-10-25 15:54:17 -0700

    Added templates folder & updated app.py.

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# Create engine
+# Does it work to point the create_engine function at a .csv?
+engine = create_engine("static/data/data_food_deserts_lookup.csv")
+
 # Step 3: Index route. This would be connected to the home page, I believe.
 @app.route("/")
 def home():
@@ -18,6 +22,11 @@ def statedata():
     
     return "I want to return JSON data... I think. I'm not sure how that then is made usable in a javascript file."
 
+@app.route("/api/v1.0/multdata")
+def multdata():
+    # I know that for an API, a session needs to be created that is using the "engine",
+    # but I don't know how to point that create_engine function to our .csv's...
+    session = Session(engine)
 
 # Boiler plate flask app code
 if __name__ == "__main__":
