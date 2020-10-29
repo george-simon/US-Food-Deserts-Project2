7e42bd2d301a43c1a7933505d4af3028fdea77cd
bandrewv <brock.vriesman@gmail.com>
2020-10-28 18:58:45 -0700

    Latest app.py.

--- app.py
+++ app.py
@@ -1,51 +1,78 @@
-# Step 1: Import Flask
+# Step 1: Import Libraries
+import numpy as np
 from flask import Flask, render_template, jsonify
 from flask_cors import CORS
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
 # Step 2: Create an app
 app = Flask(__name__)
 
 CORS(app)
 
-mult_vul_data = [{
-    "OBJECTID": 1,
-    "STATE": 41,
-    "COUNTY": "051",
-    "TRACT": "003100"
-},
-{
-    "OBJECTID": 2,
-    "STATE": 41,
-    "COUNTY": "051",
-    "TRACT": "004602"
-}]
+# mult_vul_data = [{
+#     "OBJECTID": 1,
+#     "STATE": 41,
+#     "COUNTY": "051",
+#     "TRACT": "003100"
+# },
+# {
+#     "OBJECTID": 2,
+#     "STATE": 41,
+#     "COUNTY": "051",
+#     "TRACT": "004602"
+# }]
 
-# Create engine
-# Does it work to point the create_engine function at a .csv?
-# engine = create_engine("static/data/data_food_deserts_lookup.csv")
+# Step 3: Create engine directed at PostGreSQL Database
+engine = create_engine("postgresql://localhost/food_deserts")
 
-# Step 3: Index route. This would be connected to the home page, I believe.
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+nationaldata = Base.classes.national_data
+mult_data = Base.classes.multnomah_data
+
+# Step 4: Create routes including a root level and API call routes
 @app.route("/")
 def home():
 
-    # Added render_template to root route
-    return render_template('basic.html')
+    # In the root level route, link to API call routes
+    return (
+        f"<a href='/api/v1.0/nationaldata'>National Data</a><br/>"
+        f"<a href='/api/v1.0/multdata'>Multnomah County, Oregon Data</a><br/>"
+    )
 
-# This page would be a link on the home page that would take the user to the
-# page displaying a heat map with state food desert data.
-@app.route("/statedata")
+    # Added render_template to root route
+    # return render_template('basic.html')
+
+@app.route("/api/v1.0/nationaldata")
 def statedata():  
     
-    return "I want to return JSON data... I think. I'm not sure how that then is made usable in a javascript file."
+    session = Session(engine)
+
+    results = session.query(nationaldata)
+
+    session.close()
+
+    all_results = list(np.ravel(results))
+
+    return jsonify(all_results)
 
 @app.route("/api/v1.0/multdata")
 def multdata():
-    # I know that for an API, a session needs to be created that is using the "engine",
-    # but I don't know how to point that create_engine function to our .csv's...
-    # session = Session(engine)
 
-    # Switched render_template to root route
-    return jsonify(mult_vul_data)
+    session = Session(engine)
+
+    results = session.query(mult_data)
+
+    session.close()
+
+    all_results = list(np.ravel(results))
+
+    return jsonify(all_results)
+
 
 # Boiler plate flask app code
 if __name__ == "__main__":
