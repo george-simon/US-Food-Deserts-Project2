9ca67a186cd74b2f02c11cd37b9fb37cd101240e
bandrewv <brock.vriesman@gmail.com>
2020-10-29 19:54:40 -0700

    Got data flowing through app.py file.

--- static/js/viz1.js
+++ static/js/viz1.js
@@ -1,1 +1,2 @@
 d3.json("/api/v1.0/multdata").then(data => console.log(data)).catch(error => console.log(error))
+d3.json("/api/v1.0/natdata").then(data => console.log(data)).catch(error => console.log(error))
