81818fdde54c2dcc7460fa0abf9b369322bd26bc
bandrewv <brock.vriesman@gmail.com>
2020-10-31 12:07:35 -0700

    Cleaning .js & .html files.

--- static/js/viz1.js
+++
@@ -1,2 +0,0 @@
-d3.json("/api/v1.0/multdata").then(data => console.log(data)).catch(error => console.log(error))
-d3.json("/api/v1.0/natdata").then(data => console.log(data)).catch(error => console.log(error))
