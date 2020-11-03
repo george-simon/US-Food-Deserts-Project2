a98b9706abe8424e247485ffd98a1fd7474cdebd
bandrewv <brock.vriesman@gmail.com>
2020-11-02 17:56:02 -0800

    Fixed nat_map_json.sql file.

--- resources/schema/nat_map_json.sql
+++ resources/schema/nat_map_json.sql
@@ -2,5 +2,5 @@ DROP TABLE IF EXISTS nat_map_json;
 CREATE TABLE nat_map_json (
    code         VARCHAR(10)  PRIMARY KEY 
   ,name                VARCHAR(60)
-  ,value               FLOAT IS NOT NULL
+  ,value               FLOAT
 );
